2d00d3a4b849332c32dc71982689ad833d4afe0b
Andrey <shock.work@mail.ru>
2021-09-15 18:16:17 +0300

    start using vars of env

--- src/api/weather.js
+++ src/api/weather.js
@@ -2,7 +2,7 @@ import axios from '../core/axios';
 
 class WeatherAPI {
   async getByCity(city) {
-    const { data } = await axios.get(`/weather?q=${city}&appid=9dadc0bdca73e856dec3ee4297334764&lang=ru&units=metric`);
+    const { data } = await axios.get(`/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_KEY}&lang=ru&units=metric`);
     return data;
   }
 }
